9fe08257eb0f0201c483c84f6ec1f10354f5e2a9
Maxwel <maxwelsant96@icloud.com>
2021-10-17 17:23:51 -0300

    image profile

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
 
 const Home = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, I&apos;m a full-stack developer based in Brazil
       </Box>
 
@@ -13,6 +25,24 @@ const Home = () => {
             Maxwel Santana
           </Heading>
           <p>Full-stack Developer</p>
+        </Box>
+
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="https://avatars.githubusercontent.com/u/17615307?s=96&v=4"
+            alt="Profile Image"
+          />
         </Box>
       </Box>
     </Container>
