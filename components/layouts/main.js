4bbba94bf7dca8f51176fa9f604914934c52d9d9
Maxwel <maxwelsant96@icloud.com>
2021-10-17 16:48:42 -0300

    navbar component

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import { Box, Container } from '@chakra-ui/react'
+import Navbar from '../navbar'
 
 const Main = ({ children, router }) => {
   return (
@@ -9,6 +10,7 @@ const Main = ({ children, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale" />
         <title>Maaxwel Santana - Homepage</title>
       </Head>
+      <Navbar path={router.asPath} />
       <Container maxW="container.md" pt={14}>
         {children}
       </Container>
